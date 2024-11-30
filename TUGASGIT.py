02415dbde9eefd59497cc47350fe7d47d33fa052
AriilAlHafizh <ariil.alhafizh26@gmail.com>
2024-11-30 14:06:20 +0700

    version 1

---
+++ TUGASGIT.py
@@ -0,0 +1,61 @@
+data_panen = {
+    'lokasi1' : {
+        'nama_lokasi':'Kebun A',
+        'hasil_panen' : {
+            'padi': 1200,
+            'jagung': 800,
+            'kedelai': 500
+        }
+    },
+    'lokasi2' : {
+        'nama_lokasi':'Kebun B',
+        'hasil_panen' : {
+            'padi': 1500,
+            'jagung': 900,
+            'kedelai': 450
+        }
+    },
+    'lokasi3' : {
+        'nama_lokasi':'Kebun C',
+        'hasil_panen' : {
+            'padi': 1100,
+            'jagung': 750,
+            'kedelai': 600
+        }
+    },
+    'lokasi4' : {
+        'nama_lokasi':'Kebun D',
+        'hasil_panen' : {
+            'padi': 1300,
+            'jagung': 850,
+            'kedelai': 550
+        }
+    },
+    'lokasi5' : {
+        'nama_lokasi':'Kebun E',
+        'hasil_panen' : {
+            'padi': 1400,
+            'jagung': 950,
+            'kedelai': 480
+        }
+    }
+}
+
+for i,j in data_panen.items() :
+    print(i,j)
+
+hasil = data_panen['lokasi2']['hasil_panen']['jagung']
+print(hasil)
+
+nama = data_panen['lokasi3']['nama_lokasi']
+print(nama)
+
+for nama, hasil in data_panen.items() :
+    nama_lok = hasil['nama_lokasi']
+    padi = hasil['hasil_panen']['padi']
+    kedelai = hasil['hasil_panen']['kedelai']
+
+    if padi > 1300 or kedelai > 800 :
+        print(f"lokasi {nama_lok} memerlukan perhatian khusus")
+    else :
+        print(f"lokasi {nama_lok} aman")
